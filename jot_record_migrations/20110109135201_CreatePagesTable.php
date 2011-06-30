74f7d8f0bdc78d2e5ef96cfb1b0d6b4e71900a2d
Tom Krush <t.krush@gmail.com>
2011-06-29 20:43:12 -0500

    Fixed migration.

--- jot_record_migrations/20110109135201_CreatePagesTable.php
+++ jot_record_migrations/20110109135201_CreatePagesTable.php
@@ -10,7 +10,7 @@ class CreatePagesTable
 			array('name' => 'title', 'type' => 'string', 'NOT_NULL' => false),
 			array('name' => 'keywords', 'type' => 'string'),
 			array('name' => 'description', 'type' => 'string'),
-			array('name' => 'is_public', 'type' => 'boolean', 'NOT_NULL' => false, 'default' => false),
+			array('name' => 'is_public', 'type' => 'boolean', 'NOT_NULL' => false, 'default' => 0),
 			array('name' => 'content', 'type' => 'text'),
 			MIGRATION_TIMESTAMPS
 		));		
